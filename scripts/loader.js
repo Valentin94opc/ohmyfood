f55ab45450d4c4579f83de25d93eb5a29e1ab895
valentin94opc <valentin@pluvego.co>
2023-04-15 11:35:54 +0200

    Feat: Others restaurants added

--- scripts/loader.js
+++
@@ -1,9 +0,0 @@
-const contentToHideWrapper = document.getElementById("content-to-hide-wrapper");
-const loader = document.getElementById("loader");
-
-window.addEventListener("load", function () {
-  setTimeout(function () {
-    contentToHideWrapper.classList.remove("hide-content");
-    loader.classList.add("hide-content");
-  }, 3000);
-});
